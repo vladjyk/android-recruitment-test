8922c6c89968c32b3ecb6345d31ce35535181964
hakalini <vladjyk@gmail.com>
2020-03-09 15:04:32 +0200

    Added ViewAnimationUtil

---
+++ app/src/main/java/dog/snow/androidrecruittest/util/ViewAnimationUtils.kt
@@ -0,0 +1,88 @@
+package dog.snow.androidrecruittest.util
+
+import android.view.View
+import android.view.View.GONE
+import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.Transformation
+
+
+object ViewAnimationUtils {
+
+    fun expand(v: View) {
+        v.measure(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+
+        val targetHeight = v.measuredHeight
+
+        v.layoutParams.height = 0
+        v.visibility = View.VISIBLE
+
+        val a = object : Animation() {
+
+            override fun applyTransformation(interpolatedTime: Float, t: Transformation) {
+                v.layoutParams.height =
+                    if (interpolatedTime == 1f)
+                        ViewGroup.LayoutParams.WRAP_CONTENT
+                    else
+                        (targetHeight * interpolatedTime).toInt()
+                v.requestLayout()
+            }
+
+            override fun willChangeBounds(): Boolean {
+                return true
+            }
+        }
+
+        a.duration = (targetHeight / v.context.resources.displayMetrics.density).toLong()
+        v.startAnimation(a)
+    }
+
+    fun collapse(v: View) {
+        val initialHeight = v.measuredHeight
+
+        val a = object : Animation() {
+
+            override fun applyTransformation(interpolatedTime: Float, t: Transformation) {
+                if (interpolatedTime == 1f) {
+                    v.visibility = GONE
+                } else {
+                    v.layoutParams.height =
+                        initialHeight - (initialHeight * interpolatedTime).toInt()
+                    v.requestLayout()
+                }
+            }
+
+            override fun willChangeBounds(): Boolean {
+                return true
+            }
+        }
+
+        a.duration = (initialHeight / v.context.resources.displayMetrics.density).toLong()
+        v.startAnimation(a)
+    }
+
+    fun collapseSlowly(v: View){
+        val initialHeight = v.measuredHeight
+
+        val a = object : Animation() {
+
+            override fun applyTransformation(interpolatedTime: Float, t: Transformation) {
+                if (interpolatedTime == 1f) {
+                    v.visibility = GONE
+                } else {
+                    v.layoutParams.height =
+                        initialHeight - (initialHeight * interpolatedTime).toInt()
+                    v.requestLayout()
+                }
+            }
+
+            override fun willChangeBounds(): Boolean {
+                return true
+            }
+        }
+
+        a.duration = ((initialHeight / v.context.resources.displayMetrics.density) * 12).toLong()
+        v.startAnimation(a)
+    }
+
+}
